96c5c61f507538070fa1eef99a8d92f50b392f8b
Jonathan Gallegos Alegria <jonathangallegos@outlook.fr>
2024-03-04 20:26:21 +0100

    Cours: variables d'environnement 4

--- pipelines/jenkinsfile.groovy
+++ pipelines/jenkinsfile.groovy
@@ -2,24 +2,42 @@ pipeline {
     agent any
     environment {
         SHOOL = "datascientest"
+        NAME = "Anthony"
     }
-
     stages {
         stage("Env Variables") {
             environment {
-                NAME = "Datascientest"
+                NAME = "lewis" // overrides pipeline level NAME env variable
+                BUILD_ID = "2" // overrides the default BUILD_ID
             }
+
             steps {
-                echo "SHOOL = ${env.SHOOL}"
-                echo "NAME = ${env.NAME}"
+                echo "SHOOL = ${env.SHOOL}" // prints "SHOOL = bar"
+                echo "NAME = ${env.NAME}" // prints "NAME = lewis"
+                echo "BUILD_ID =  ${env.BUILD_ID}" // prints "BUILD_ID = 2"
 
                 script {
-                    env.TEST_VARIABLE = "some test value"
+                    env.SOMETHING = "1" // creates env.SOMETHING variable
                 }
-                echo "TEST_VARIABLE = ${env.TEST_VARIABLE}"
+            }
+        }
 
-                withEnv(["ANOTHER_ENV_VAR=here is some value"]) {
-                    echo "ANOTHER_ENV_VAR = ${env.ANOTHER_ENV_VAR}"
+        stage("Override Variables") {
+            steps {
+                script {
+                    env.SHOOL = "I LOVE DATASCIENTEST!" // it can't override env.SHOOL declared at the pipeline (or stage) level
+                    env.SOMETHING = "2" // it can override env variable created imperatively
+                }
+
+                echo "SHOOL = ${env.SHOOL}" // prints "SHOOL = bar"
+                echo "SOMETHING = ${env.SOMETHING}" // prints "SOMETHING = 2"
+
+                withEnv(["SHOOL=DEV UNIVERSITY"]) { // it can override any env variable
+                    echo "SHOOL = ${env.SHOOL}" // prints "SHOOL = DEV UNIVERSITY"
+                }
+
+                withEnv(["BUILD_ID=1"]) {
+                    echo "BUILD_ID = ${env.BUILD_ID}" // prints "BUILD_ID = 1"
                 }
             }
         }
